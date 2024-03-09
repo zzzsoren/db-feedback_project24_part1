df62d7a7a8a257dd80819dc8e74ea44490772a8c
zzzsoren <soren.rosendahl@outlook.dk>
2024-03-09 14:39:11 +0100

    sale with most value

--- 2_highest_value_sale.sql
+++ 2_highest_value_sale.sql
@@ -1,3 +1,17 @@
-SELECT saleid, value*quantity
-FROM sale_of_product 
-WHERE value*quantity = (SELECT MAX(value*quantity) from sale_of_product);
+-- SELECT saleid, value*quantity
+-- FROM sale_of_product 
+-- WHERE value*quantity = (SELECT MAX(value*quantity) from sale_of_product);
+
+-- Finder det salg med laveste beløb
+(SELECT saleid, SUM(value*quantity) AS salevalue
+FROM sale_of_product
+GROUP BY saleid
+ORDER BY salevalue ASC
+LIMIT 1)
+UNION
+-- Finder det salg med højeste beløb
+(SELECT saleid, SUM(value*quantity) AS salevalue
+FROM sale_of_product
+GROUP BY saleid
+ORDER BY salevalue DESC
+LIMIT 1)
