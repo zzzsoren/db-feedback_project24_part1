e33ea44e462866f31a56160f28abab21615d6305
zzzsoren <soren.rosendahl@outlook.dk>
2024-03-09 14:43:52 +0100

    categories with min and max number og sold items

--- 3_categories_products_sold.sql
+++ 3_categories_products_sold.sql
@@ -1,3 +1,13 @@
-SELECT categoryid, sum(quantity) AS NumberOfSoldItems
+-- Finder den category der sælger færrest vare.
+(SELECT categoryid, sum(quantity) AS numberofsolditems
 FROM sale_of_product NATURAL JOIN product
-GROUP BY categoryid;
+GROUP BY categoryid
+ORDER BY numberofsolditems ASC
+LIMIT 1)
+UNION
+-- Finder den category der sælger flest vare.
+(SELECT categoryid, sum(quantity) AS numberofsolditems
+FROM sale_of_product NATURAL JOIN product
+GROUP BY categoryid
+ORDER BY numberofsolditems DESC
+LIMIT 1)
